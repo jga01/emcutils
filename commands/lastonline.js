f5067f670e1e55d5ec6d97a255d84f0352a54d6e
Jamestiago <tiagogoossenbra@gmail.com>
2023-08-05 10:51:04 -0300

    change townsindanger to lastonline

---
+++ commands/lastonline.js
@@ -0,0 +1,74 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, ComponentType, codeBlock } = require('discord.js');
+const { fetchNationMayors } = require('../utils/fetch');
+const { sortMayors } = require('../utils/sortMayors');
+const { formatMayors } = require('../utils/formatMayors');
+const constants = require('../constants');
+
+const COMMAND_NAME = 'lastonlinemayors';
+const COMMAND_DESCRIPTION = 'See when mayors were last online';
+const EMBED_TITLE = 'When mayors were last online';
+const CONTENT_FORMAT = 'ini';
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName(COMMAND_NAME)
+		.setDescription(COMMAND_DESCRIPTION),
+	async execute(interaction) {
+		const row = () => new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('ascending')
+					.setEmoji('⬆️')
+					.setStyle(ButtonStyle.Primary),
+				new ButtonBuilder()
+					.setCustomId('descending')
+					.setEmoji('⬇️')
+					.setStyle(ButtonStyle.Primary),
+			);
+
+		function customEmbedBuilder(title, description, footerText) {
+			const embed = new EmbedBuilder()
+				.setColor(0x0099FF)
+				.setTitle(title)
+				.setDescription(description)
+				.setFooter({ text: `${footerText}`, iconURL: constants.ICON_URL });
+			return embed;
+		}
+
+		const mayors = await fetchNationMayors('Brazil');
+		const formattedMayors = formatMayors(mayors);
+
+		let content = formattedMayors.join('\n');
+		content = codeBlock(CONTENT_FORMAT, content);
+		
+		const message = await interaction.reply({ embeds: [customEmbedBuilder(EMBED_TITLE, content, constants.BOT_NAME)], components: [row()], fetchReply: true });
+
+		const collector = message.createMessageComponentCollector({ ComponentType: ComponentType.Button, time: 5 * 60 * 1000 });
+
+		collector.on('collect', async i => {
+			if (i.customId === 'ascending') {
+				const ascendingMayors = sortMayors(mayors, true);
+				const formattedSortedMayors = formatMayors(ascendingMayors);
+
+				content = formattedSortedMayors.join('\n');
+				content = codeBlock(CONTENT_FORMAT, content);
+
+				await i.update({ embeds: [customEmbedBuilder(EMBED_TITLE, content, constants.BOT_NAME)], components: [row()] });
+			}
+			else if (i.customId === 'descending') {
+				const ascendingMayors = sortMayors(mayors, false);
+				const formattedSortedMayors = formatMayors(ascendingMayors);
+
+				content = formattedSortedMayors.join('\n');
+				content = codeBlock(CONTENT_FORMAT, content);
+
+				await i.update({ embeds: [customEmbedBuilder(EMBED_TITLE, content, constants.BOT_NAME)], components: [row()] });
+			}
+		});
+
+		collector.on('end', collected => {
+			console.log(`Collected ${collected.size} items`);
+			interaction.editReply({ embeds: [customEmbedBuilder(EMBED_TITLE, content, constants.BOT_NAME)], components: [] });
+		});
+	},
+};
--- utils/formatMayors.js
+++ utils/formatMayors.js
@@ -3,7 +3,7 @@ const { convertUTCtoDays } = require("./convertUTCtoDays");
 const formatMayors = (mayors) => {
     const mappedMayors = mayors.map((mayor) => {
         let days = convertUTCtoDays(mayor.timestamps.lastOnline);
-        return `${mayor.name} of ${mayor.town} was last online ${days} ${days === 1 ? 'day' : 'days'} ago`;
+        return `[${mayor.name}] of [${mayor.town}] was last online [${days}] ${days === 1 ? 'day' : 'days'} ago`;
     });
     return mappedMayors;
 }
